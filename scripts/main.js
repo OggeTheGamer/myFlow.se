b8e57fd015a6a799f8699c0e298d78ddd92e1bf6
Oscar Freij <otg020313@gmail.com>
2018-10-18 19:52:39 +0200

    Added New post

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+'use strict';
 function postSearch() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("postSearchBar");
@@ -12,4 +13,18 @@ function postSearch() {
             li[i].style.display = "none";
         }
     }
+}
+
+function calculator3dDistance() {
+  var x1, x2, y1, y2, z1, z2, h, distance;
+  x1 = document.getElementById('x1').value;
+  x2 = document.getElementById('x2').value;
+  y1 = document.getElementById('y1').value;
+  y2 = document.getElementById('y2').value;
+  z1 = document.getElementById('z1').value;
+  z2 = document.getElementById('z2').value;
+  console.log(x1 + x2 + y1 + y2 + z1 + z2);
+  h = ((Math.pow((x1-x2),2)) + (Math.pow((x1-x2),2)) + (Math.pow((x1-x2),2)));
+  distance = Math.sqrt(h);
+  document.getElementById('calcAnswer').innerHTML = "Answer: " + distance + " Units";
 }
